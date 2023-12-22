7f7d42226184de210247597cabad2bbb4ba073e6
Sony <jsaulhernandez98@gmail.com>
2023-12-21 20:54:35 -0600

    tag: adding components to index

---
+++ src/app/shared/components/index.ts
@@ -0,0 +1,9 @@
+import { PokeItemComponent } from './poke-item/poke-item.component';
+import { PokeLoaderComponent } from './poke-loader/poke-loader.component';
+import { PokeTextComponent } from './poke-text/poke-text.component';
+
+export const SHARED_COMPONENTS = [
+    PokeItemComponent,
+    PokeTextComponent,
+    PokeLoaderComponent,
+];
