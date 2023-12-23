58b90c45dcb2aeb749941581e14421c585f2f1f7
Sony <jsaulhernandez98@gmail.com>
2023-12-23 06:17:26 -0600

    tag: adding evolution to pokemon detail

--- src/app/modules/init/pages/detail/detail.component.ts
+++ src/app/modules/init/pages/detail/detail.component.ts
@@ -2,7 +2,14 @@ import { Component, OnInit, OnDestroy, inject } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { lastValueFrom } from 'rxjs';
 
-import { DataPokemon, Move, Type } from 'src/app/data/api/ResponseApi';
+import {
+    Common,
+    DataPokemon,
+    EvolutionChain,
+    Move,
+    Specie,
+    Type,
+} from 'src/app/data/api/ResponseApi';
 
 import { ApiService } from 'src/app/data/services/api.service';
 import { LoaderService } from 'src/app/shared/services/loader.service';
@@ -15,10 +22,14 @@ import { LoaderService } from 'src/app/shared/services/loader.service';
 export class DetailComponent implements OnInit, OnDestroy {
     api$ = inject(ApiService);
 
-    pokemonName: string = '';
     isLoading = this.loader$.loading$;
     pokemon!: DataPokemon;
     moves: string[] = [];
+    evolutions: {
+        item: Common;
+        color: Common;
+        generation: Common;
+    }[] = [];
 
     constructor(
         private activatedRoute: ActivatedRoute,
@@ -28,23 +39,22 @@ export class DetailComponent implements OnInit, OnDestroy {
 
     async ngOnInit() {
         // getting param from URL
-        this.pokemonName =
-            this.activatedRoute.snapshot.paramMap.get('pokemon') ?? '';
-        await this.getPokemonByName();
+        await this.getPokemonByName(
+            this.activatedRoute.snapshot.paramMap.get('pokemon') ?? ''
+        );
         if (this.pokemon)
             this.moves = this.transformMovesKey(this.pokemon.moves);
     }
 
     ngOnDestroy(): void {}
 
-    async getPokemonByName() {
+    async getPokemonByName(pokemonName: string) {
         this.loader$.show();
         try {
-            const response = this.api$.getDataPokemonByName<DataPokemon>(
-                `pokemon/${this.pokemonName}`
+            const dataBase = await this.getDataPokemon<DataPokemon>(
+                pokemonName,
+                'pokemon'
             );
-
-            const dataBase = (await lastValueFrom(response)) ?? [];
             await this.getOtherData(dataBase);
             this.loader$.hide();
         } catch (error) {
@@ -53,7 +63,105 @@ export class DetailComponent implements OnInit, OnDestroy {
         }
     }
 
+    async getDataPokemon<T extends unknown>(
+        pokemon: string | number,
+        path: string
+    ) {
+        const response = this.api$.getDataPokemonByName<T>(
+            `${path}/${pokemon}`
+        );
+
+        const dataBase = (await lastValueFrom(response)) ?? ({} as T);
+        return dataBase;
+    }
+
     async getOtherData(data: DataPokemon) {
+        // getting specie
+        const responseSpecie = this.api$.getDataPokemonByUrl<Specie>(
+            data.species.url
+        );
+        let dataSpecie =
+            (await lastValueFrom(responseSpecie)) ?? ({} as Specie);
+
+        // getting evolutions
+        const responseEvolution = this.api$.getDataPokemonByUrl<EvolutionChain>(
+            dataSpecie.evolution_chain.url
+        );
+        let dataEvolution =
+            (await lastValueFrom(responseEvolution)) ?? ({} as EvolutionChain);
+
+        const firstID = dataEvolution.chain.species.url.split('/')[6];
+        const firstDataPokemon = await this.getDataPokemon<DataPokemon>(
+            firstID,
+            'pokemon'
+        );
+        const firstSpecie = await this.getDataPokemon<Specie>(
+            firstID,
+            'pokemon-species'
+        );
+
+        this.evolutions.push({
+            item: {
+                name: dataEvolution.chain.species.name,
+                url:
+                    firstDataPokemon.sprites?.other['official-artwork']
+                        ?.front_default ?? '',
+            },
+            color: firstSpecie.color,
+            generation: firstSpecie.generation,
+        });
+
+        if (dataEvolution.chain.evolves_to.length > 0) {
+            const secondDataEvolution =
+                dataEvolution.chain.evolves_to[0].species;
+            const secondID = secondDataEvolution.url.split('/')[6];
+            const secondDataPokemon = await this.getDataPokemon<DataPokemon>(
+                secondID,
+                'pokemon'
+            );
+            const secondSpecie = await this.getDataPokemon<Specie>(
+                firstID,
+                'pokemon-species'
+            );
+
+            this.evolutions.push({
+                item: {
+                    name: secondDataEvolution.name,
+                    url:
+                        secondDataPokemon.sprites?.other['official-artwork']
+                            ?.front_default ?? '',
+                },
+                color: secondSpecie.color,
+                generation: secondSpecie.generation,
+            });
+
+            if (dataEvolution.chain.evolves_to[0].evolves_to.length > 0) {
+                const thirdDataEvolution =
+                    dataEvolution.chain.evolves_to[0].evolves_to[0].species;
+                const thirdID = thirdDataEvolution.url.split('/')[6];
+                const thirdDataPokemon = await this.getDataPokemon<DataPokemon>(
+                    thirdID,
+                    'pokemon'
+                );
+                const thirdSpecie = await this.getDataPokemon<Specie>(
+                    firstID,
+                    'pokemon-species'
+                );
+
+                this.evolutions.push({
+                    item: {
+                        name: thirdDataEvolution.name,
+                        url:
+                            thirdDataPokemon.sprites?.other['official-artwork']
+                                ?.front_default ?? '',
+                    },
+                    color: thirdSpecie.color,
+                    generation: thirdSpecie.generation,
+                });
+            }
+        }
+
+        console.log(this.evolutions);
         this.pokemon = data;
     }
 
