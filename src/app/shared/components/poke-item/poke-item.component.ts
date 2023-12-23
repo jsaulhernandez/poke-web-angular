50a949ce4df03abf73c4ebf6ced31b63e2029edf
Sony <jsaulhernandez98@gmail.com>
2023-12-22 18:02:46 -0600

    tag: switching option to redirect to detail page

--- src/app/shared/components/poke-item/poke-item.component.ts
+++ src/app/shared/components/poke-item/poke-item.component.ts
@@ -1,4 +1,5 @@
 import { Component, Input } from '@angular/core';
+import { Router } from '@angular/router';
 import { DataPokemon } from 'src/app/data/api/ResponseApi';
 
 @Component({
@@ -10,10 +11,16 @@ export class PokeItemComponent {
     @Input() data!: DataPokemon;
     @Input() showAdditionalData: boolean = false;
 
+    constructor(private router: Router) {}
+
     generatePokeNumber(pokeNumber: number): string {
         if (pokeNumber < 10) return `#00${pokeNumber}`;
         else if (pokeNumber < 100) return `#0${pokeNumber}`;
 
         return `#${pokeNumber}`;
+    }
+
+    onRedirectToDetail() {
+        this.router.navigate(['/poke/detail', this.data.name]);
     }
 }
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { RouterModule } from '@angular/router';
 import { ReactiveFormsModule, FormsModule } from '@angular/forms';
 
 import { MaterialModule } from './material.module';
@@ -9,13 +8,7 @@ import * as shared from './components/index';
 
 @NgModule({
     declarations: [...shared.SHARED_COMPONENTS],
-    imports: [
-        CommonModule,
-        ReactiveFormsModule,
-        FormsModule,
-        MaterialModule,
-        RouterModule,
-    ],
+    imports: [CommonModule, ReactiveFormsModule, FormsModule, MaterialModule],
     exports: [
         ...shared.SHARED_COMPONENTS,
         ReactiveFormsModule,
