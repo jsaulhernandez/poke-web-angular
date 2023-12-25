bb6161356f802c2718d91634f6cbe6cc11910e2c
Sony <jsaulhernandez98@gmail.com>
2023-12-25 14:26:13 -0600

    tag: updating selected pokemon component

--- src/app/modules/admin/components/selected-pokemon/selected-pokemon.component.ts
+++ src/app/modules/admin/components/selected-pokemon/selected-pokemon.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { DataPokemon } from 'src/app/data/api/ResponseApi';
 
 @Component({
-  selector: 'app-selected-pokemon',
-  templateUrl: './selected-pokemon.component.html',
-  styleUrls: ['./selected-pokemon.component.scss']
+    selector: 'app-selected-pokemon',
+    templateUrl: './selected-pokemon.component.html',
+    styleUrls: ['./selected-pokemon.component.scss'],
 })
 export class SelectedPokemonComponent {
+    @Input() pokeData!: DataPokemon;
 
+    getElement() {
+        return this.pokeData.types.length > 0
+            ? this.pokeData.types[0].type.name
+            : 'unknown';
+    }
 }
