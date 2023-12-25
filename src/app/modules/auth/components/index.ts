47aa5df3a156b213de6843916505056a79b71c1a
Sony <jsaulhernandez98@gmail.com>
2023-12-24 20:34:38 -0600

    tag: moving user card component to shared module

--- src/app/modules/auth/components/index.ts
+++ src/app/modules/auth/components/index.ts
@@ -1,3 +1,1 @@
-import { UserCardComponent } from './user-card/user-card.component';
-
-export const COMPONENTS = [UserCardComponent];
+export const COMPONENTS = [];
--- src/app/shared/components/index.ts
+++ src/app/shared/components/index.ts
@@ -2,10 +2,12 @@ import { PokeButtonComponent } from './poke-button/poke-button.component';
 import { PokeItemComponent } from './poke-item/poke-item.component';
 import { PokeLoaderComponent } from './poke-loader/poke-loader.component';
 import { PokeTextComponent } from './poke-text/poke-text.component';
+import { UserCardComponent } from './user-card/user-card.component';
 
 export const SHARED_COMPONENTS = [
     PokeItemComponent,
     PokeTextComponent,
     PokeLoaderComponent,
     PokeButtonComponent,
+    UserCardComponent,
 ];
